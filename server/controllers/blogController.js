2b50903283b7daacea525248bc1bfea2c1ce89d7
reasat-rafio <rafioar96@gmail.com>
2020-11-17 00:35:05 +0600

    blog backend complete

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -25,8 +25,8 @@ export const blogs_get = async (req, res) => {
 };
 
 // @DESC    post a new blog
-// @METHOD POST
-// ROUTE /post
+// @METHOD  POST
+// ROUTE    /blogs/post
 export const blogs_post = async (req, res) => {
    try {
       // setting the user in database as the logged in user id from the server
@@ -41,7 +41,85 @@ export const blogs_post = async (req, res) => {
          data: newBlog,
       });
    } catch (error) {
-      // server response if it failed to get the data
+      // server response if it fail to get the data
+      res.status(500).json({
+         success: false,
+         error: error.message,
+      });
+   }
+};
+
+// DESC     delete a blog
+// METHOD   DELETE
+// ROUTE    blogs/delete/:id
+export const blogs_delete = async (req, res) => {
+   // getting the id from request url
+   const id = req.params.id;
+   try {
+      // finding the blog
+      const blog = await Blog.findById(id);
+
+      // if that blog dont exist
+      if (!blog) {
+         return res.status(404).json({
+            success: false,
+            error: "Blog dont exist",
+         });
+      }
+
+      if (blog.user == req.cookies.id) {
+         // Deleting the blog from database
+         const deletedBlog = await Blog.remove({ _id: id });
+         // server response at success
+         res.status(200).json({
+            success: true,
+            data: deletedBlog,
+         });
+      }
+   } catch (error) {
+      // server response if it fail to get the data
+      res.status(500).json({
+         success: false,
+         error: error.message,
+      });
+   }
+};
+
+//    @DESC    Update or edit a existing blog
+//    @METHOD  PETCH
+//    @ROUTE   /blogs/edit/:id
+export const blogs_patch = async (req, res) => {
+   // getting the id from request url
+   const id = req.params.id;
+   try {
+      // finding the blog
+      const blog = await Blog.findById(id);
+      // if that blog dont exist
+      if (!blog) {
+         // server response at success
+         return res.status(404).json({
+            success: false,
+            error: "Blog dont exist",
+         });
+      }
+
+      if (blog.user == req.cookies.id) {
+         // Editing the blog from database
+         const updateBlog = await Blog.findByIdAndUpdate(
+            id,
+            { ...req.body, id },
+            { new: true }
+         );
+
+         // server response at success
+         res.status(201).json({
+            success: true,
+            data: updateBlog,
+         });
+      }
+   } catch (error) {
+      console.log(error);
+      // server response if it fail to get the data
       res.status(500).json({
          success: false,
          error: error.message,
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { blogs_get, blogs_post } from "../controllers/blogController.js";
+import {
+   blogs_get,
+   blogs_post,
+   blogs_delete,
+   blogs_patch,
+} from "../controllers/blogController.js";
 
 const router = express.Router();
 
 router.get("/", blogs_get);
 router.post("/post", blogs_post);
+router.delete("/delete/:id", blogs_delete);
+router.patch("/edit/:id", blogs_patch);
 
 export default router;
